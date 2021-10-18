d707731cf6168c00ac9209e21b39ab347a9202dd
Leah <jacksonl22@mail.wlu.edu>
2021-10-18 16:29:16 -0400

    merging with Tash's commit

--- new_table.R
+++ new_table.R
@@ -9,9 +9,13 @@ tables <- lapply(filenames, readStateTables)
 aggregate <- do.call(rbind, tables)
 aggregate
 
+# added a column with state names
 aggregate$state_name <- substr(aggregate$file, 16, 17)
 
-
+# deleted useless columns: incident id, clearance, age_range_low (same as age), age_range_high (always 0), file name
+library(dplyr)
+aggregate <- select(aggregate, -3, -16, -18, -19)
+aggregate <- select(aggregate, -15)
 
 #renaming values as race names in a new column
 
