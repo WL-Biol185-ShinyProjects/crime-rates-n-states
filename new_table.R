ae645b7a5955400f02f742ea3b757e08bdcbcfea
Natasha Gengler <genglern22@mail.wlu.edu>
2021-10-18 15:59:20 -0400

    Deleted useless columns

--- new_table.R
+++ new_table.R
@@ -9,4 +9,19 @@ tables <- lapply(filenames, readStateTables)
 aggregate <- do.call(rbind, tables)
 aggregate
 
+
+# added a column with state names
 aggregate$state_name <- substr(aggregate$file, 16, 17)
+
+# deleted useless columns: incident id, clearance, age_range_low (same as age), age_range_high (always 0), file name
+library(dplyr)
+aggregate <- select(aggregate, -3, -16, -18, -19)
+aggregate <- select(aggregate, -15)
+
+
+
+
+
+
+
+
