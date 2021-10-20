7417eacbb34ea55aad6bc13820bcc15807ff504b
Natasha Gengler <genglern22@mail.wlu.edu>
2021-10-20 15:01:44 -0400

    Made a script to decode offender type on our aggregate table

---
+++ replace_offender_type_data.R
@@ -0,0 +1,13 @@
+
+
+offense_type_id_key <- read.csv(NIBRS_OFFENSE_TYPE.CSV)
+
+offense_type <- offense_type_id_key$OFFENSE_NAME
+  
+names(offense_type) <- offense_type_id_key$OFFENSE_TYPE_ID
+
+aggregate$offense_type <- offense_type[as.character(aggregate$OFFENSE_TYPE_ID)]
+
+View(aggregate)
+
+
