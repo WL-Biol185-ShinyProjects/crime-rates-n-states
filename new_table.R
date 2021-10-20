dceb69f21a6b469838c9c379a314b7e56f023a98
Leah <jacksonl22@mail.wlu.edu>
2021-10-20 14:58:44 -0400

    FINALLY! Renaming number key codes as actual race names

--- new_table.R
+++ new_table.R
@@ -20,9 +20,11 @@ aggregate <- select(aggregate, -15)
 #renaming values as race names in a new column
 
 NIBRS_ARRESTEE_AL <- read.csv("~/crime-rates-n-states/NIBRS_ARRESTEE_AL.csv")
-a <- c("Unknown", "White", "Black or African American", "American Indian or Alaska Native", "Asian", "Asian, Native Hawaiian, or Other Pacific Islander", "Chinese", "Japanese", "Native Hawaiian or Other Pacific Islander", "Other", "Multiple", "Not Specified") 
-names(a) <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "98", "99")
-a[as.character(c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 99))]
-NIBRS_ARRESTEE_AL$RACE <- as.character(NIBRS_ARRESTEE_AL$RACE_ID)
-NIBRS_ARRESTEE_AL$RACE <- a[NIBRS_ARRESTEE_AL$RACE_ID]
+
+race <- c("Unknown", "White", "Black or African American", "American Indian or Alaska Native", "Asian", "Asian, Native Hawaiian, or Other Pacific Islander", "Chinese", "Japanese", "Native Hawaiian or Other Pacific Islander", "Other", "Multiple", "Not Specified") 
+
+names(race) <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "98", "99")
+
+NIBRS_ARRESTEE_AL$RACE <- race[as.character(NIBRS_ARRESTEE_AL$RACE_ID)]
+
 View(NIBRS_ARRESTEE_AL)
