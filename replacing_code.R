625dfb6f8399e2baf7d4545a91643fe97eee0378
Hieu Nguyen <nguyenh22@mail.wlu.edu>
2021-10-24 23:55:02 -0400

    Testing out creating map using ggplot2

--- replacing_code.R
+++ replacing_code.R
@@ -1,8 +1,28 @@
-key <- read.csv("NIBRS_OFFENSE_TYPE.CSV")
-key
+library(ggplot2)
+library(maps)
 
-arrest_type <- key$OFFENSE_NAME
-names(arrest_type) <- key$OFFENSE_TYPE_ID
-arrest_type[1]
+ggplot(data   = aggregate_edited, 
+       mapping = aes(x     = long, 
+                     y     = lat, 
+                     group = group, 
+                     fill  = aggregate_edited$offense_type)) + 
+  
+  geom_polygon(color = "gray90",
+             size = 0.1) + 
+  
+  coord_map(projection = "albers", 
+            lato = 39, 
+            lat1 = 45) + 
+  
+  labs(title = "Offense Type") + 
+  
+  labs(fill = "Percent") + 
+  
+  scale_fill_gradient(low = "white", 
+                      high = "#CB454A") + 
+  
+  labs(title = "Offense Type") + 
+  
+  labs(fill= "Percentage")
 
-aggregate$ARREST_TYPE <- 
+  
