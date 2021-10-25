9e99c9eedef936dd2aaf1d6220c6432b9870d825
Hieu Nguyen <nguyenh22@mail.wlu.edu>
2021-10-25 15:37:55 -0400

    create new table with total number of each offense type by states

--- replacing_code.R
+++ replacing_code.R
@@ -2,33 +2,17 @@ library(ggplot2)
 library(maps)
 library(dplyr)
 
-#####
-filenames <- list.files(pattern = ".csv")
-readStateTables <- function(file){
-  d <-read.csv(file)
-  d$file <- file
-  d
-}
+aggregate_edited %>%
+  group_by(state_name, offense_type) %>%
+  summarise(totalcrime = n()) %>%
+  View()
 
-tables    <- lapply(filenames, readStateTables)
-aggregate <- do.call(rbind, tables)
-########
 
-listofstates <- c('AL', 'AR', 'AZ', 'CO', 'CT', 'DC', 'DE', 'GA', 'HI', 'IA', 'ID', 'IL', 
-                  'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 
-                  'NB', 'NC', 'ND', 'NH', 'NM', 'NV', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 
-                  'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY')
+#listofstates <- c('AL', 'AR', 'AZ', 'CO', 'CT', 'DC', 'DE', 'GA', 'HI', 'IA', 'ID', 'IL', 
+#                  'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 
+#                  'NB', 'NC', 'ND', 'NH', 'NM', 'NV', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 
+#                  'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY')
 
-totalcasebystate <- function(file1){
-  b <- length(aggregate_edited$state_name == file1)
-  b$state <- file1
-  b$totalcase <- b
-  b
-}
-
-table1 <- lapply(listofstates, totalcasebystate)
-newtable <- do.call(rbind, table1)
-View(newtable)
 
 ggplot(data   = aggregate_edited, 
        mapping = aes(x     = long, 
