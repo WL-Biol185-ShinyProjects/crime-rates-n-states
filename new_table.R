2002211d68ae7275edd808f57b7680f55b7694a0
Natasha Gengler <genglern22@mail.wlu.edu>
2021-10-25 15:05:17 -0400

    debugging offense type key

--- new_table.R
+++ new_table.R
@@ -24,7 +24,7 @@ aggregate$race <- race[as.character(aggregate$RACE_ID)]
 
 
 #replacing offender type data
-offense_type_id_key    <- read.csv(NIBRS_OFFENSE_TYPE.CSV)
+offense_type_id_key    <- read.csv(NIBRS_OFFENSE_TYPE_real.csv)
 offense_type           <- offense_type_id_key$OFFENSE_NAME
 names(offense_type)    <- offense_type_id_key$OFFENSE_TYPE_ID
 aggregate$offense_type <- offense_type[as.character(aggregate$OFFENSE_TYPE_ID)]
