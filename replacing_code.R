6bba317df4f4a94a698ac9e5d4283b5323218c03
Hieu Nguyen <nguyenh22@mail.wlu.edu>
2021-10-27 15:32:17 -0400

    data rangling

--- replacing_code.R
+++ replacing_code.R
@@ -3,39 +3,25 @@ library(maps)
 library(dplyr)
 library(tidyverse)
 
-percentagetable <- aggregate_edited %>%
-  group_by(state_name, offense_type) %>%
-  summarise(crimes = n()) %>%
-  as.data.frame()
-
-percentagetable <- aggregate_edited %>%
-  group_by(state_name, offense_type) %>%
-  summarise(crimes = n()) %>%
-  as.data.frame()
-
 table <- aggregate_edited %>%
-  group_by(state_name, offense_type == "Shoplifting") %>%
-  summarise(crimes = n()) %>%
+  group_by(state_name, offense_type$Shoplifting) %>%
+  summarise(shoplifting = n()) %>%
   as.data.frame()
 
 View(table)
+
+
+totalcrimebystate <- aggregate_edited %>%
+  group_by(state_name) %>%
+  summarise(totalcrimes = n()) %>%
+  
+View(totalcrimebystate)
 
 ## need to debug this
 listofstates <- c('AL', 'AR', 'AZ', 'CO', 'CT', 'DC', 'DE', 'GA', 'HI', 'IA', 'ID', 'IL', 
                   'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 
                   'NB', 'NC', 'ND', 'NH', 'NM', 'NV', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 
                   'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY')
-
-percentagetable$totalbystate <- aggregate_edited %>%
-  group_by(state_name) %>%
-  summarise(totalcrimes = n()) %>%
-  View()
-  
-  
-
-View(percentagetable)
-
-
 
 
 ggplot(data   = aggregate_edited, 
