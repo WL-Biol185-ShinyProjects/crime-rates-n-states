6f6763c6416d0a97258f698da5788fb53920d6d8
Natasha Gengler <genglern22@mail.wlu.edu>
2021-10-27 15:15:45 -0400

    edited new tabs

---
+++ server.R
@@ -0,0 +1,34 @@
+library(shiny)
+
+# globalMean <- read.delim(".../global-mean.txt", header = TRUE)
+
+
+function(input, output) {
+  x <- reactive({
+    if (input$distribution == "normal") {
+      rnorm(100, input$number)
+    } else {
+      runif(100, max = input$number)
+    }
+    
+  })
+  
+  
+  output$hist <- renderPlot({
+    
+    col <- if (input$distribution == "normal") {
+      "pink"
+    } else { "green"
+    }
+    hist(x(), col = col)
+    
+  })
+  
+  
+  output$summary <- renderText({
+    summary(x())
+    
+    
+  })
+  
+}
