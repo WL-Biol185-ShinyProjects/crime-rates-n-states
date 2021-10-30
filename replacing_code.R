0f3cee823ed0b54792be67b683a22676769b920f
Hieu Nguyen <nguyenh22@mail.wlu.edu>
2021-10-29 23:16:02 -0400

    change formate

--- replacing_code.R
+++ replacing_code.R
@@ -1,43 +1,16 @@
-library(ggplot2)
-library(maps)
 library(dplyr)
 library(tidyverse)
-library(mapproj)
 
-table <- aggregate_edited %>%
+shoplifting_case <- aggregate_edited %>%
   group_by(state_name, offense_type) %>%
   summarise(shoplifting = n()) %>%
   filter(offense_type == "Shoplifting")
 
-totalcrimebystate <- aggregate_edited %>%
+total_crime_by_state <- aggregate_edited %>%
   group_by(state_name) %>%
-  summarise(totalcrimes = n())
+  summarise(total_crimes = n())
 
-newtable <- left_join(table, totalcrimebystate)
-newtable$percentofshoplifting <- newtable$shoplifting / newtable$totalcrimes
-View(newtable)
+percentage_table <- left_join(shoplifting_case, total_crime_by_state)
+percentage_table$percent_of_shoplifting <- percentage_table$shoplifting / percentage_table$total_crimes
+View(percentage_table)
 
-us_states <- map_data("state")
-head(us_states)
-dim(us_states)
-
-
-ggplot(data   = us_states, 
-       mapping = aes(x     = long, 
-                     y     = lat, 
-                     group = group, 
-                     fill  = region)) + 
-  
-  geom_polygon(color = "gray90", size = 0.1) +
-  coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
-  guides(fill = FALSE)
-  
-  labs(title = "Offense Type") + 
-  
-  labs(fill = "Percent") + 
-  
-  scale_fill_gradient(low = "white", 
-                      high = "#CB454A")
-
-
-  
