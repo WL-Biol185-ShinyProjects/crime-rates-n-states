7ff4b87a98f31f0507f207b672ee8840f4b22127
Hieu Nguyen <nguyenh22@mail.wlu.edu>
2021-10-29 23:09:47 -0400

    attempt using ggplot for map

--- ggplot_usamap.R
+++ ggplot_usamap.R
@@ -1,7 +1,32 @@
 library(ggplot2)
-library(shiny)
+library(maps)
+library(dplyr)
+library(tidyverse)
+library(mapproj)
 
-ggplot(data = aggregate, mapping = aes(x = long, y = lat, group = group, fill = ))
+
+us_states <- map_data("state")
+head(us_states)
+dim(us_states)
+
+
+ggplot(data   = us_states, 
+       mapping = aes(x     = long, 
+                     y     = lat, 
+                     group = group, 
+                     fill  = region)) + 
+  
+  geom_polygon(color = "gray90", size = 0.1) +
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
+  guides(fill = FALSE)
+
+labs(title = "Offense Type") + 
+  
+  labs(fill = "Percent") + 
+  
+  scale_fill_gradient(low = "white", 
+                      high = "#CB454A")
+
 
 
 
