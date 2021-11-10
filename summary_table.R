b242676403eec4650f627ace18ca05ec2bec7491
Natasha Gengler <genglern22@mail.wlu.edu>
2021-11-09 22:08:28 -0500

    changing it so summary_table isn't in our main files

--- summary_table.R
+++ summary_table.R
@@ -49,7 +49,7 @@ state_full_name <- state_converter$State
 names(state_full_name) <- state_converter$Abbreviation
 summary_table$state_full_name <- state_full_name[as.character(summary_table$state_name)]
 
-write.csv(summary_table,"summary_table.csv", row.names = TRUE)
+#write.csv(summary_table,"summary_table_folder/summary_table.csv", row.names = TRUE)
 
 
 
