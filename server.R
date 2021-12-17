b81fbfe12c444e42690f9335df5724c3ea363a76
Natasha Gengler <genglern22@mail.wlu.edu>
2021-12-16 21:16:21 -0500

    formatting

--- server.R
+++ server.R
@@ -69,7 +69,7 @@ function(input, output) {
         theme_minimal()                                    + 
         theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.title = element_blank())
     }) 
-}
+
 
   output$downloadData <- downloadHandler(
 
@@ -80,6 +80,7 @@ function(input, output) {
        write.csv(aggregate_edited, file)
      }
  )
+}
   
   
   
